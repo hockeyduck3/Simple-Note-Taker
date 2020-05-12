9f891c44b734f656aa5e686c627b9c63b41dd670
LJ Spencer <hockeyduck3@gmail.com>
2020-05-11 18:16:10 -0600

    Added a server post method

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,32 @@
 // Dependency
 var data = require('../db/db.json');
 var fs = require('fs');
+var {v4: uuidv4} = require('uuid')
 
 // Server function
 module.exports = function(server) {
     server.get('/api/notes', function(req, res) {
         return res.json(data);
+    });
+
+    server.post('/api/notes', function(req, res) {
+        let id = uuidv4();
+
+        let note = {
+            title: req.body.title,
+            text: req.body.text,
+            id: id
+        };
+
+
+        fs.readFile('./db/db.json', 'utf8', function(err, res) {
+            if (err) throw err;
+
+            let json = JSON.parse(res);
+
+            json.push(note);
+
+            fs.writeFileSync('./db/db.json', JSON.stringify(json))
+        })
     });
 }
